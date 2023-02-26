a308b3a6c19b5665822fa0fd1304637b8d68164f
unknown <dy.yev.solovyov@gmail.com>
2023-02-26 18:19:16 +0200

    some fixes with i18next

--- src/services/i18next.ts
+++ src/services/i18next.ts
@@ -1,16 +1,48 @@
 import i18n from "i18next";
-import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(Backend)
-	.use(LanguageDetector)
-	.use(initReactI18next)
-	.init({
-		fallbackLng: "en",
-		interpolation: {
-			escapeValue: false,
+const resources = {
+	en: {
+		translation: {
+			pageTitle: "NEWS PORTAL",
+			uaButton: "UA",
+			enButton: "EN",
+			homeButton: "HOME",
+			newsButton: "NEWS",
+			profileButton: "PROFILE",
+			loginButton: "LOGIN",
+			logoutButton: "LOGOUT",
+			loadButton: "LOAD MORE",
+			emailLabel: "Email",
+			passwordLabel: "Password",
+			footer: "Its my test task for the company AlterEGO",
 		},
-	});
+	},
+	ua: {
+		translation: {
+			pageTitle: " ПОРТАЛ НОВИН",
+			uaButton: "УКР",
+			enButton: "АНГЛ",
+			homeButton: "ГОЛОВНА",
+			newsButton: "НОВИНИ",
+			profileButton: "ПРОФІЛЬ",
+			loginButton: "УВІЙТИ",
+			logoutButton: "ВИЙТИ",
+			loadButton: "ЗАВАНТАЖИТИ БІЛЬШЕ",
+			emailLabel: "Електронна пошта",
+			passwordLabel: "Пароль",
+			footer: "Це моє тестове завдання для компанії AlterEGO",
+		},
+	},
+};
+
+i18n.use(initReactI18next).init({
+	resources,
+	lng: "en",
+	fallbackLng: "en",
+	interpolation: {
+		escapeValue: false,
+	},
+});
 
 export default i18n;
